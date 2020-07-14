9460d1ee5528e2008dafb06195d7de9037d49911
linym <2279378438@qq.com>
2020-07-14 11:02:49 +0800

    HASE-Welleness 源代码初始化

---
+++ pages/index/index.js
@@ -0,0 +1,51 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    active:0
+  },
+  
+  onLoad: function () {
+   
+    
+  },
+  onShow: function () {
+    this.setData({
+      active: 0
+    })
+  },
+  onShareAppMessage: function (options) {
+    let shareObj = {
+      　　　　title: "",
+      　　　　path: '/pages/index/index',
+      imageUrl: '/images/tabBar/timg.jpg',
+      　　　　success: function (res) {
+        　　　　　　// 转发成功之后的回调
+        　　　　　　if (res.errMsg == 'shareAppMessage:ok') {
+        　　　　　　}
+      　　　　},
+      　　　　fail: function () {
+        　　　　　　// 转发失败之后的回调
+        　　　　　　if (res.errMsg == 'shareAppMessage:fail cancel') {
+          　　　　　　　　// 用户取消转发
+        　　　　　　} else if (res.errMsg == 'shareAppMessage:fail') {
+          　　　　　　　　// 转发失败，其中 detail message 为详细失败信息
+        　　　　　　}
+      　　　　}
+      // 　complete:fucntion(){
+      // 　// 转发结束之后的回调（转发成不成功都会执行）
+      // 　}
+    　　}
+    　　// 来自页面内的按钮的转发
+    　　if (options.from == 'button') {
+      let eData = options.target.dataset;
+      　　　　console.log(eData.name);     // shareBtn
+      　　　　// 此处可以修改 shareObj 中的内容
+      　　　　shareObj.path = '/pages/btnname/btnname?btn_name=' + eData.name;
+    　　}
+    　　// 返回shareObj
+    　　return shareObj;
+  }
+})
---
+++ pages/login/index.js
@@ -0,0 +1,127 @@
+// pages/login/index.js
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    url:''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+    this.setData({
+      url:options.url
+    })
+    console.log(options)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  phoneNumberLogin(data) {
+    const parms = {
+      encryptedData: data.encryptedData,
+      iv: data.iv,
+      openId: app.globalData.userInfo.openId,
+
+    }
+    wx.showLoading({
+      title: 'loading...',
+    })
+    wx.request({
+      method: 'post',
+      url: app.globalData.baseUrl + '/remote/register/miniProgram/add',
+      header: {
+        "Content-Type": "application/json;charset=UTF-8"
+      },
+      data: parms,
+      success: (res) => {
+        if (res.data.code === 200) {
+          app.globalData.token = res.data.token          
+          wx.redirectTo({
+            url: this.data.url,
+            complete: () => {
+
+            }
+          })
+        } else {
+          wx.showModal({
+            showCancel: false,
+            content: res.message,
+            success: (res) => { }
+          })
+        }
+        wx.hideLoading()
+      }
+    })
+  },
+  getPhoneNumber(e) { //获取电话信息     
+    if (e.detail.errMsg === 'getPhoneNumber:ok') {
+     
+      this.phoneNumberLogin(e.detail)
+    } else {
+      wx.redirectTo({
+        url: '../index/index',
+        complete: () => {
+
+        }
+      })
+    }
+
+  },
+  stopLogin() {
+    wx.redirectTo({
+      url: '../index/index'
+    })
+  },
+})
