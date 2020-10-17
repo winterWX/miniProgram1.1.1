59b65ffe241c7c4666da59646d874cbaa73b779e
winterWX <wangxiang081457@163.com>
2020-10-17 16:04:53 +0800

    fix: 删除无用代码

--- pages/signAppointment/index.js
+++ pages/signAppointment/index.js
@@ -21,48 +21,6 @@ Page({
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
   clickHandleHK: function() {
     wx.navigateTo({
       url: '../offlineAppointment/index',
@@ -70,20 +28,6 @@ Page({
     })
   },
   scanCode: function() {
-   /*  wx.scanCode({
-      onlyFromCamera: false,
-      scanType: 'qrCode',
-      success(res) {
-        console.log(res)
-      },
-      fail(){
-        wx.showToast({
-          title: '扫码失败',
-          icon: 'loading',
-          duration: 1500
-       })
-      }
-    }) */
     wx.navigateTo({
       url: '../scan/index',
     })
