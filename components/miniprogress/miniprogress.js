d9b2b60dbe25ff75726929559ed9b41b80d37353
winterWX <wangxiang081457@163.com>
2020-10-26 12:37:41 +0800

    run fer code

--- components/miniprogress/miniprogress.js
+++ components/miniprogress/miniprogress.js
@@ -26,18 +26,17 @@ Component({
        numStep :0
     },
     ready: function () {
-        // clearInterval(timer);
-        // const query = this.createSelectorQuery()
-        // query.select('#ring').boundingClientRect(res => {
-        //     this.drawRing('ring', res.width, res.height,this.data.stepNum)
-        // }).exec()
+        const query = this.createSelectorQuery()
+        query.select('#ringTop').boundingClientRect(res => {
+            this.topDrawRing('ringTop', res.width, res.height)
+        }).exec()
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
-        drawRing: function (canvasId, width, height,stepNum) {
+        topDrawRing: function(canvasId, width, height){
             var context = wx.createCanvasContext(canvasId, this)
             // 外层圆环
             context.beginPath()
@@ -46,13 +45,29 @@ Component({
             context.setLineCap('round')
             context.setStrokeStyle('#ebeeeb')
             context.textAlign = 'center' //文字居中
-            context.font = '14px Arial'
+            context.font = '16px Arial'
             context.fillStyle = '#7D7D7D' //字体颜色
-            context.fillText('今日步数',width / 2,25)
+            context.fillText('今日步数',width / 2,22)
             context.stroke()
-            
+            context.draw()
+        },
+        drawRing: function (canvasId, width, height,stepNum) {
+            var context = wx.createCanvasContext(canvasId, this)
+            // // 外层圆环
+            // context.beginPath()
+            // context.arc(width / 2, 40, width / 2 - 70, 1 * Math.PI, 2 * Math.PI,true)
+            // context.setLineWidth(12)
+            // context.setLineCap('round')
+            // context.setStrokeStyle('#ebeeeb')
+            // context.textAlign = 'center' //文字居中
+            // context.font = '14px Arial'
+            // context.fillStyle = '#7D7D7D' //字体颜色
+            // context.fillText('今日步数',width / 2,25)
+            // context.stroke()
+
             let stepData = 0;
             let angle = Math.ceil(stepNum / 100);
+            console.log("angleangle",angle)
             timer = setInterval(() => {
                 if(stepData === angle){
                     clearInterval(timer);
@@ -68,7 +83,7 @@ Component({
                     context.font = '40px Arial'
                     context.fillStyle = '#333333' //字体颜色
                     console.log('stepNum===',stepNum)
-                    context.fillText( stepNum+'' , width / 2,70);
+                    context.fillText( stepNum+'' , width / 2,72);
                     context.stroke()
 
                     // 指示器
