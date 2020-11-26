91cfb40e2e9382f657c61e0ee196775f4e150401
winterWX <wangxiang081457@163.com>
2020-11-26 17:51:12 +0800

    fix: 答题活动标题更改

--- pages/healthKnowledge/index.js
+++ pages/healthKnowledge/index.js
@@ -116,7 +116,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
     let { id, goodsId, title } = options;
     let activityId = id || goodsId;
     wx.setNavigationBarTitle({ title });
