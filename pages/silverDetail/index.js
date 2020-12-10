63d14f37fd31cc76d16c4695e84695449354887d
subinbin <wangxiang081457@163.com>
2020-12-10 10:30:31 +0800

    fix: 去掉内容里面的标签

--- pages/silverDetail/index.js
+++ pages/silverDetail/index.js
@@ -123,7 +123,7 @@ Page({
       let max = Math.max(...mileStonesIntegral);
       let result = 0;
       if (integral < min) {
-        result = 2.5;
+        result = 3;
       } else if (integral > max) {
         result = 100;
       } else {
